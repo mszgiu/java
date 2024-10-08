a076edc928d76d354cc0049e6a69f2609c74f5e1
Giulia Marques <0001068970@senaimgaluno.com.br>
2024-10-08 15:51:09 -0300

    Adicionando Arquivo

---
+++ Repeticao/C.java
@@ -0,0 +1,24 @@
+//desenvolvido por Giulia Marques - 23/09/24
+import java.util.Scanner;
+
+public class Exe_05 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int num;
+		int quadrado;
+		int cubo;
+
+		System.out.println("Digite um numero: ");
+		num = scanner.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+			quadrado = i * i;
+			cubo = i * i * i;
+			System.out.printf("%d %d %d%n", i, quadrado, cubo);
+		}
+
+	}
+
+}
