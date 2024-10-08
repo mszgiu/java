567f771db920e7cc1158127c80e0db07574858e3
Giulia Marques <0001068970@senaimgaluno.com.br>
2024-10-08 15:42:12 -0300

    Adicionando Arquivo

---
+++ Basico/C.java
@@ -0,0 +1,26 @@
+
+public class Testes_operadores {
+
+	public static void main(String[] args) {
+		// A)
+	double conta_a =  2 * 6/3;
+	System.out.println(conta_a);
+	
+		// B)
+	double conta_b =  3 + 2 * 4;
+	System.out.println(conta_b);
+	
+		// C)
+	double conta_c =  (3+2) * 4;
+	System.out.println(conta_c);
+	
+		// D)
+	double conta_d =  60 / (3 + 2) * 4;
+	System.out.println(conta_d);
+	
+		// E)
+	double conta_e =  60 / ((3 + 2) * 4);
+	System.out.println(conta_e);
+	// Não era necessario declarar, poderia ter colocado so dentro do prinln
+	}
+}
