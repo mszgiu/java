4fa44cf395222bca21d6887b70b1d6418789ddb4
Giulia Marques <0001068970@senaimgaluno.com.br>
2024-10-08 15:41:56 -0300

    Adicionando Arquivo

---
+++ Basico/B.java
@@ -0,0 +1,25 @@
+//Codigo desenvolvido por Giulia Marques - 18/09/24
+import java.util.Scanner;
+
+public class Soma_Numeros_Inteiro {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int num1;
+		int num2;
+		int soma;
+
+		System.out.println("Digite um número: ");
+		num1 = scanner.nextInt();
+		System.out.println("Digite outro número: ");
+		num2 = scanner.nextInt();
+        
+		soma = num1 + num2;
+		
+		scanner.close();
+		
+		System.out.println("A soma dos números è: " + num1 + " + " + num2 + " = " + soma);
+	}
+
+}
