a1565852ae71415350b94af86c5f3216021ec8fb
Giulia Marques <0001068970@senaimgaluno.com.br>
2024-10-08 15:50:47 -0300

    Adicionando Arquivo

---
+++ Repeticao/B.java
@@ -0,0 +1,34 @@
+//desenvolvido por Giulia Marques - 23/09/24
+import java.util.Scanner;
+
+public class Exe_02 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = 0;
+	
+		System.out.println("Digite a quantidade de numeros que deseja ler:");
+		num = scanner.nextInt();
+
+		int dentro = 0;
+		int fora = 0;
+	
+		for (int i = 1; i <= num; i++) {
+			System.out.print("Digite o valor " + i + ":");
+			int X = scanner.nextInt();
+
+			if (X >= 10 && X <= 20) {
+				dentro++;
+			} else {
+				fora++;
+			}
+		}
+
+		System.out.println("Dentro: " + dentro);
+		System.out.println("Fora: " + fora);
+	}
+
+	}
+
+
