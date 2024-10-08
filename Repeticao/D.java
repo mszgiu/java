b5426fd7764ede78e8e6663a2b9ad32a8fbe2321
Giulia Marques <0001068970@senaimgaluno.com.br>
2024-10-08 15:51:35 -0300

    Adicionando Arquivo

---
+++ Repeticao/D.java
@@ -0,0 +1,32 @@
+//desenvolvido por Giulia Marques - 23/09/24
+import java.util.Scanner;
+
+public class Exe_03 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int num = 2 ;
+		int num1;
+		int num2;
+	
+		System.out.println("Digite um numero: ");
+		num1 = scanner.nextInt();
+		System.out.println("Digite outro numero: ");
+		num2 = scanner.nextInt();
+
+		for (int i = 1; i < num; i++) {
+			if (num2 == 0) {
+				System.out.println("Divisão impossivel");
+			}
+
+			else {
+				System.out.println("Resultado: " + (num1/num2) );
+			}
+		}
+         
+		scanner.close();
+		
+	}
+
+}
